f8cb41186a49710e349c96868bf3628d80daa641
guido <guido.guarnieri@studenti.unicampania.it>
2019-03-20 17:33:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/testnav/Home_Fragment.kt
@@ -0,0 +1,40 @@
+package com.example.testnav
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_home_.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class Home_Fragment : Fragment() {
+
+    override fun onCreateView(
+        /**evento che si scatena quando la vista viene creata**/
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnVal.setOnClickListener(){ Navigation.findNavController(view).navigate(R.id.action_home_Fragment2_to_figlio_Fragment22)}
+}
+
+
+
